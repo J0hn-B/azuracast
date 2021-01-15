d9263e889fb14c5b5790798f3ec7dc081a5bdb98
J0hn-B <connect-wsman@hotmail.com>
2021-01-15 14:20:51 +0000

    testing app

---
+++ app/web_radio_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Configure your web radio station
+
+# Create directory
+mkdir -p /var/azuracast
+cd /var/azuracast
+
+# Install server
+curl -fsSL https://raw.githubusercontent.com/AzuraCast/AzuraCast/master/docker.sh >docker.sh
+chmod a+x docker.sh
+./docker.sh install
